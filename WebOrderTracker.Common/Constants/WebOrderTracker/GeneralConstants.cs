793f3ead10b34c6f9ffa440b9ff557c7f7991ac5
Gerardo Gamo <jerrygpstrat@gmail.com>
2026-07-04 16:02:41 -0600

    code refactor

--- WebOrderTracker.Common/Constants/WebOrderTracker/GeneralConstants.cs
+++ WebOrderTracker.Common/Constants/WebOrderTracker/GeneralConstants.cs
@@ -2,6 +2,6 @@
 {
     public static class GeneralConstants
     {
-        public const string RemoteDbConnection = "data source=A2NWPLSK14SQL-v02.shr.prod.iad2.secureserver.net,1433;Database=WOManager;User Id=WoUserDB;Password=WoUser@vd2026; MultipleActiveResultSets=true; TrustServerCertificate=True;";
+        public const string RemoteDbConnection = "";
     }
 }

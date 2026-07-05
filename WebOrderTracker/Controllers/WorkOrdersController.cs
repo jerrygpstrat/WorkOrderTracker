7d0d7c0b81387a3aa3ca36561622d11a3bbccf14
Gerardo Gamo <jerrygpstrat@gmail.com>
2026-07-05 15:00:46 -0600

    Initial bootstrap table implementation

--- WebOrderTracker/Controllers/WorkOrdersController.cs
+++ WebOrderTracker/Controllers/WorkOrdersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using WebOrderTracker.Business.Services;
 
 namespace WebOrderTracker.Controllers
@@ -6,14 +7,30 @@ namespace WebOrderTracker.Controllers
     public class WorkOrdersController : BaseController
     {
 
-       public WorkOrdersController(WorkOrderService workOrderService) : base(workOrderService) { }
+        public WorkOrdersController(WorkOrderService workOrderService) : base(workOrderService) { }
 
         public async Task<IActionResult> Index()
         {
-            var activeOrders = await GetActiveOrders();
+
 
 
             return View();
+        }
+
+
+        /// <summary>
+        /// Get all active orders
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<JsonResult> GetAllActiveOrders()
+        {
+            var activeOrders = await GetActiveOrders();
+            return new JsonResult(new
+            {
+                rows = activeOrders.ToList(),
+                total = activeOrders != null ? activeOrders.Count() : 0
+            });
         }
     }
 }
